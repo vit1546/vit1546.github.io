c3b4b5b360dc6075780fc51e0c0f2184ac1053f1
admin <admin@admins-MacBook-Pro-3.local>
2016-07-21 17:52:31 +0300

    new

--- Kaiser/kaiser_files/emailmdhidecancel.js
+++
@@ -1,6 +0,0 @@
- var theInputs = document.getElementsByTagName("input");
- for (i=0; i<theInputs.length; i++) {
- 	if (theInputs[i].name == "cancel") {
-	theInputs[i].style.display = "none";
-	}
- }
